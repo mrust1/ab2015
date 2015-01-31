666e1c30b9b24955a0de48a7e428bf6ad512cc6d
mrust1 <mehmetctkya@gmail.com>
2015-01-31 14:33:22 +0200

    first commit

---
+++ spec/factories/admin_profiles.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :admin_profile do
+    first_name "MyString"
+last_name "MyString"
+gsm "MyString"
+admin nil
+  end
+
+end
---
+++ spec/factories/user_profiles.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user_profile do
+    first_name "MyString"
+last_name "MyString"
+gsm "MyString"
+user nil
+  end
+
+end
